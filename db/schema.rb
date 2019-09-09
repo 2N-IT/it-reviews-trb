de596c5aa52a6e0f280b1117f69d24f575266443
pyrin andrii <pyrin.andrii@gmail.com>
2019-09-09 21:58:11 +0300

    Add index to Users email and make it unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_125327) do
+ActiveRecord::Schema.define(version: 2019_09_09_185122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_09_07_125327) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
