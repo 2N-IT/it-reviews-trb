5705f87fa019f70edd10bc665795002138a1cee0
pyrin andrii <pyrin.andrii@gmail.com>
2019-09-24 21:24:52 +0300

    Change User::Contract::Create namespacing and add email regexp validation

--- app/concepts/user/contract/create.rb
+++ app/concepts/user/contract/create.rb
@@ -1,12 +1,14 @@
-require 'reform/form/dry'
+class User < ApplicationRecord
+  module Contract
+    class Create < Reform::Form
+      include Dry
+      property :email
 
-module User::Contract
-  class Create < Reform::Form
-    feature Reform::Form::Dry
-    property :email
+      VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
-    validation :default do
-      required(:email).filled(:str?)
+      validation :default do
+        required(:email).filled(format?: VALID_EMAIL_REGEX)
+      end
     end
   end
 end
