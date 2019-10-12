acd94d7582d900431951bf4db289f5bd9f06fcbf
pyrin andrii <pyrin.andrii@gmail.com>
2019-10-12 15:25:27 +0300

    Add active to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_185122) do
+ActiveRecord::Schema.define(version: 2019_10_12_122205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_185122) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
