d7646817e14f8bb3a7f3fd9fc328145241734736
pyrin andrii <pyrin.andrii@gmail.com>
2019-10-30 23:34:05 +0200

    Update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  def new
+    run User::Create::Present
+  end
+
   def create
     run User::Create
+  end
+
+  def activate
+    run User::Activate
   end
 end
