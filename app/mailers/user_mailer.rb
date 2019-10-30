9a64d8297497ea3ebbf2f2da7ca363a5b89bf0e6
pyrin andrii <pyrin.andrii@gmail.com>
2019-10-30 23:35:55 +0200

    Update user mailer, add template for welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,6 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(model)
-    mail(
-      to:           model.email,
-      body:         body,
-      subject:      'Welcome!',
-      content_type: 'text/html'
-    )
-  end
-
-  def body
-    'Welcome to My Awesome Site'
+  def welcome_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Registration Confirmation')
   end
 end
-
-# TODO: create link to users/activate and change active: to true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create]
+  default_url_options :host => "example.com"
+  
+  resources :users, only: %i[create] do
+    get :activate, action: :activate, on: :member
+  end
+
 end
