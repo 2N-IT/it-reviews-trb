fd646e82563dab9d64526f247c80fc6bbc93743a
pyrin andrii <pyrin.andrii@gmail.com>
2019-11-04 22:50:01 +0200

    Use nested class/module definitions

--- app/concepts/user/operation/activate.rb
+++ app/concepts/user/operation/activate.rb
@@ -1,29 +1,31 @@
-class User::Activate < Trailblazer::Operation
-  step :model!
-  fail :user_does_not_exist!
-  step :activate_user!
-  fail :user_is_active_already!
-  step :persist!
+class User
+  class Activate < Trailblazer::Operation
+    step :model!
+    fail :user_does_not_exist!
+    step :activate_user!
+    fail :user_is_active_already!
+    step :persist!
 
-  def model!(options, params:, **)
-    options['model'] = User.find_by(activation_token: params[:activation_token])
-  end
+    def model!(options, params:, **)
+      options['model'] = User.find_by(activation_token: params[:activation_token])
+    end
 
-  def user_does_not_exist!(options, params:, **)
-    options['result.model'] = 'Wrong activation token ' +
-                              "#{params[:activation_token]}!"
-  end
+    def user_does_not_exist!(options, params:, **)
+      options[:errors] = 'Wrong activation token ' +
+                                "#{params[:activation_token]}!"
+    end
 
-  def activate_user!(options, model:, **)
-    options['model'].active = true unless model.active
-  end
+    def activate_user!(options, model:, **)
+      options['model'].active = true unless model.active
+    end
 
-  def user_is_active_already!(options, params:, **)
-    options['result.model'] ||= 'User with activation token ' +
-                              "#{params[:activation_token]} is already active!"
-  end
+    def user_is_active_already!(options, params:, **)
+      options[:errors] ||= 'User with activation token ' +
+                                "#{params[:activation_token]} is already active!"
+    end
 
-  def persist!(options, **)
-    options['model'].save!
+    def persist!(options, **)
+      options['model'].save!
+    end
   end
 end
