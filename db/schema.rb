04e8742d056e734b78331f6c57537277ec3083e9
pyrin andrii <pyrin.andrii@gmail.com>
2019-11-06 23:43:16 +0200

    Add has_secure_password to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_122205) do
+ActiveRecord::Schema.define(version: 2019_11_06_213526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 2019_10_12_122205) do
     t.string "last_name"
     t.string "nickname"
     t.string "email", null: false
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active", default: false
     t.string "activation_token"
+    t.string "password_digest"
     t.index ["activation_token"], name: "index_users_on_activation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
