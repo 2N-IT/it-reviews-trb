1a42de391536c1315d9eb947cf9887f9ee85ab2a
pyrin andrii <pyrin.andrii@gmail.com>
2019-11-28 23:10:19 +0200

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,21 @@
 class UsersController < ApplicationController
   def new
     run User::Operation::Create::Present
+    render cell(User::Cell::New, @form), layout: false
   end
 
   def create
-    run User::Operation::Create
+    run User::Operation::Create do |result|
+      return redirect_to users_path(id: result['model'].id)
+    end
+    p @form
+    render cell(User::Cell::New, @form), layout: false
   end
 
   def activate
-    run User::Operation::Activate
+    run User::Operation::Activate do |result|
+      return redirect_to 'smt.......'
+    end
+    render cell(User::Cell::Welcome, @form), layout: false
   end
 end
